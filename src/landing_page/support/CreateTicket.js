3730447ff88176be133d7030bf0ec0678ee0765d
CodeWithMrunal <mrunalanandache2020@gmail.com>
2025-08-18 15:46:53 +0530

    updated support page

--- src/landing_page/support/CreateTicket.js
+++ src/landing_page/support/CreateTicket.js
@@ -2,9 +2,77 @@ import React from "react";
 
 function CreateTicket(){
     return (
-        <>
-        <h1>CreateTicket</h1>
-        </>
+        <div className="container">
+            <h1>To create a ticket, select a relevant topic</h1>
+            <div className="row">
+                <div className="col-4 mt-5 pt-5">
+                    <h4><i class="fa-solid fa-circle-plus"></i> Account Opening</h4>
+                    <div className="mt-3 ps-4" style={{lineHeight:"2.5"}}>
+                        <a href="/" className="text-decoration-none ">Resident individual</a><br/>
+                        <a href="/" className="text-decoration-none ">Minor</a><br/>
+                        <a href="/" className="text-decoration-none ">Non Resident Indian (NRI)</a><br/>
+                        <a href="/" className="text-decoration-none ">Company, Partnership, HUF and LLP</a><br/>
+                        <a href="/" className="text-decoration-none ">Glossary</a><br/>
+                    </div>
+                </div>
+                <div className="col-4 mt-5 pt-5">
+                    <h4><i class="fa-solid fa-user"></i> Your Zerodha Account</h4>
+                    <div className="mt-3 ps-4" style={{lineHeight:"2.5"}}>
+                        <a href="/" className="text-decoration-none">Your Profile</a><br/>
+                        <a href="/" className="text-decoration-none">Account modification</a><br/>
+                        <a href="/" className="text-decoration-none">Client Master Report (CMR) and Depository Participant (DP)</a><br/>
+                        <a href="/" className="text-decoration-none">Nomination</a><br/>
+                        <a href="/" className="text-decoration-none">Transfer and conversion of securities</a><br/>
+                    </div>
+                </div>
+                <div className="col-4 mt-5 pt-5">
+                    <h4><i class="fa-brands fa-fly"></i> Kite</h4>
+                    <div className="mt-3 ps-4" style={{lineHeight:"2.5"}}>
+                        <a href="/" className="text-decoration-none">IPO</a><br/>
+                        <a href="/" className="text-decoration-none">Trading FAQs</a><br/>
+                        <a href="/" className="text-decoration-none">Margin Trading Facility (MTF) and Margins</a><br/>
+                        <a href="/" className="text-decoration-none">Charts and orders</a><br/>
+                        <a href="/" className="text-decoration-none">Alerts and Nudges</a><br/>
+                        <a href="/" className="text-decoration-none">General</a><br/>
+                    </div>
+                </div>
+            </div>
+            <div className="row mb-5 pb-5">
+                <div className="col-4 mt-5 pt-5">
+                    <h4><i class="fa-solid fa-indian-rupee-sign"></i> Funds</h4>
+                    <div className="mt-3 ps-4" style={{lineHeight:"2.5"}}>
+                        <a href="/" className="text-decoration-none">Add money</a><br/>
+                        <a href="/" className="text-decoration-none">Withdraw money</a><br/>
+                        <a href="/" className="text-decoration-none">Add bank accounts</a><br/>
+                        <a href="/" className="text-decoration-none">eMandates</a><br/>
+                    </div>
+                </div>
+                <div className="col-4 mt-5 pt-5">
+                    <h4><i class="fa-solid fa-terminal"></i> Console</h4>
+                    <div className="mt-3 ps-4" style={{lineHeight:"2.5"}}>
+                        <a href="/" className="text-decoration-none">Portfolio</a><br/>
+                        <a href="/" className="text-decoration-none">Corporate actions</a><br/>
+                        <a href="/" className="text-decoration-none">Funds statement</a><br/>
+                        <a href="/" className="text-decoration-none">Reports</a><br/>
+                        <a href="/" className="text-decoration-none">Profile</a><br/>
+                        <a href="/" className="text-decoration-none">Segments</a><br/>
+                    </div>
+                </div>
+                <div className="col-4 mt-5 pt-5">
+                    <h4><i class="fa-solid fa-coins"></i> Coin</h4>
+                    <div className="mt-3 ps-4" style={{lineHeight:"2.5"}}>
+                         <a href="/" className="text-decoration-none">Mutual funds</a><br/>
+                         <a href="/" className="text-decoration-none">National Pension Scheme (NPS)</a><br/>
+                         <a href="/" className="text-decoration-none">Fixed Deposit (FD)</a><br/>
+                         <a href="/" className="text-decoration-none">Features on Coin</a><br/>
+                         <a href="/" className="text-decoration-none">Payments and Orders</a><br/>
+                         <a href="/" className="text-decoration-none">General</a><br/>
+                    </div>
+                </div>
+                <div className="col-4"></div>
+                <div className="col-4"></div>
+            </div>
+        </div>
     );
 }
 
--- src/landing_page/support/Hero.js
+++ src/landing_page/support/Hero.js
@@ -2,9 +2,33 @@ import React from "react";
 
 function Home(){
     return (
-        <>
-        <h1>Home</h1>
-        </>
+        <div className="container-fluid mb-5" style={{backgroundColor:"rgb(57, 125, 208)", color:"white"}}>
+            <div className="container py-5 px-5">
+                <div className="d-flex flex-row justify-content-between align-items-center">
+                    <h1 className="fs-3">Support Portal</h1>
+                    <a href="/" style={{color:"white"}} className="fs-5">Track Tickets</a>
+                </div>
+                <div className="row">
+                    <div className="col-6 mt-5">
+                        <h1 className="fs-3">Search for an answer or browse help topics to create a ticket</h1>
+                        <input className="support_input" placeholder="Eg: how do i activate F&O, why is my order getting rejected.."/><br/>
+                        <a href="/" className="text-white me-3">Track account opening </a>
+                        <a href="/" className="text-white me-3">Track segment activation </a>
+                        <a href="/" className="text-white me-3">Intraday margins </a>
+                        <a href="/" className="text-white me-3">Kite user manual</a>
+                    </div>
+                    <div className="col-2 mt-5"></div>
+
+                    <div className="col-4 mt-5">
+                        <h1 className="fs-3">Featured</h1>
+                        <ol>
+                            <li><a href="/" className="text-white fs-5">Current Takeovers and Delisting - January 2024</a></li>
+                            <li><a href="/" className="text-white fs-5">Latest Intraday leverages - MIS & CO</a></li>
+                        </ol> 
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
